17fe7000798cdb4547b861be93fa996a0abbb445
github_Amandaptr44 <amanda.putri.ft22@mail.umy.ac.id>
2024-11-21 16:26:04 +0700

    Initial commit

---
+++ app/src/main/java/com/example/simpleviewmodel/model/Mahasiswa.kt
@@ -0,0 +1,8 @@
+package com.example.simpleviewmodel.model
+
+data class Mahasiswa(
+    val nama: String = "",
+    val gender: String ="",
+    val alamat: String ="",
+    val noTelepon: String = ""
+)
